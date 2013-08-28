07ad471039db52e91956c18dfe74dfcef6931ba8
DaegiKim <rmrhtdms@gmail.com>
2013-08-27 23:43:55 -0400

    add review dummy text & home page renewal

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -5,11 +5,17 @@ import play.mvc.Result;
 import securesocial.core.Identity;
 import securesocial.core.java.SecureSocial;
 import views.html.home.index;
+import views.html.home.review;
 
 @SecureSocial.SecuredAction
 public class Home extends Controller {
     public static Result index() {
         Identity user = (Identity) ctx().args.get(SecureSocial.USER_KEY);
         return ok(index.render(user));
+    }
+
+    public static Result review() {
+        Identity user = (Identity) ctx().args.get(SecureSocial.USER_KEY);
+        return ok(review.render(user));
     }
 }
