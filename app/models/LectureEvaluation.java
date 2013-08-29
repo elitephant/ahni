4af24268ed850a9d2349b5b5f542e520491e9747
DaegiKim <rmrhtdms@gmail.com>
2013-08-29 12:06:57 -0400

    주석 추가, 강의 평가 목록 가져올 때 10개 제한

--- app/models/LectureEvaluation.java
+++ app/models/LectureEvaluation.java
@@ -12,27 +12,31 @@ import javax.persistence.Id;
 import java.util.List;
 
 public class LectureEvaluation {
-    @Id
-    @ObjectId
-    public String id;
-    @Constraints.Required
-    public String lectureName;
-    @Constraints.Required
-    public String professorName;
-    @Constraints.Required
-    public String comment;
-    @Constraints.Required
-    public int rating;
-    public String userId;
-    public DateTime dateTime;
-
     private static JacksonDBCollection<LectureEvaluation, String> coll = MongoDB.getCollection("LectureEvaluation", LectureEvaluation.class, String.class);
 
+    @Id
+    @ObjectId
+    public String id;               //MongoDB Object ID
+    @Constraints.Required
+    public String lectureName;      //강의명
+    @Constraints.Required
+    public String professorName;    //교수명
+    @Constraints.Required
+    public String comment;          //수강후기
+    @Constraints.Required
+    public int rating;              //평가점수
+    public String userId;           //유저 아이디
+    public DateTime dateTime;       //작성 시간
+
     public static List<LectureEvaluation> all() {
-        return LectureEvaluation.coll.find().sort(new BasicDBObject("dateTime",-1)).toArray();
+        //시간으로 내림차순 정렬하여 최근것 부터 보여줌, 10개
+        return LectureEvaluation.coll.find().limit(10).sort(new BasicDBObject("dateTime",-1)).toArray();
     }
 
     public static void create(LectureEvaluation lectureEvaluation, Identity user) {
+        //현재 시스템 시간과 유저아이디를 set 해주고 MongoDB에 Insert
+        //TODO: 시스템 시간이 4시간 더 크게 들어감...
+        //TODO: save할 때 중복확인 할 것인가? 한 유저가 같은 강의를 여러번 평가하면...안되는데
         lectureEvaluation.dateTime = DateTime.now();
         lectureEvaluation.userId = user.identityId().userId();
         LectureEvaluation.coll.save(lectureEvaluation);
