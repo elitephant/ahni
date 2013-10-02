69608b66f039faf06f0177e23607941281b2f67b
DaegiKim <rmrhtdms@gmail.com>
2013-10-01 23:29:43 -0400

    elitephant 페이지 추가

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -5,11 +5,16 @@ import play.mvc.Result;
 import securesocial.core.Identity;
 import securesocial.core.java.SecureSocial;
 import views.html.home.index;
+import views.html.home.elitephant;
 
-@SecureSocial.SecuredAction
 public class Home extends Controller {
+    @SecureSocial.SecuredAction
     public static Result index() {
         Identity user = (Identity) ctx().args.get(SecureSocial.USER_KEY);
         return ok(index.render(user));
+    }
+
+    public static Result elitephant() {
+        return ok(elitephant.render());
     }
 }
