2e34837eec369743c36af8bb80b1a85f4b3e5b17
DaegiKim <rmrhtdms@gmail.com>
2013-10-30 14:58:44 -0400

    font-family 변경, 주석 추가

--- app/services/util/ThousandsSeparator.java
+++ app/services/util/ThousandsSeparator.java
@@ -6,6 +6,11 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class ThousandsSeparator {
+    /**
+     * int 숫자를 받아서 1000 단위로 콤마 "," 찍은 문자열을 반환
+     * @param number
+     * @return
+     */
     public static String parse(int number) {
         DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
         DecimalFormatSymbols symbols = formatter.getDecimalFormatSymbols();
